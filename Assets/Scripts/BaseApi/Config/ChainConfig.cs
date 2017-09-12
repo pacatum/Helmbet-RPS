4107c7275e06b151d76285181b132f3106f8a7e0
Admin <admin@pacatum.com>
2017-09-12 16:32:06 +0300

    Add testnet to config.

--- Assets/Scripts/BaseApi/Config/ChainConfig.cs
+++ Assets/Scripts/BaseApi/Config/ChainConfig.cs
@@ -22,13 +22,14 @@ namespace Base.Config {
 
 
 		static Dictionary<string, NetworkParameters> networks = new Dictionary<string, NetworkParameters> {
-			{ "BitShares",      new NetworkParameters( "BTS",   "BTS",  "4018d7844c78f6a6c41c6a552b898022310fc5dec06da467ee7905a8dad512c8" ) },
-			{ "Muse",           new NetworkParameters( "MUSE",  "MUSE", "45ad2d3f9ef92a49b55c2227eb06123f613bb35dd08bd876f2aea21925a67a67" ) },
-			{ "Test",           new NetworkParameters( "TEST",  "TEST", "39f5e2ede1f8bc1a3a54a7914414e3779e33193f1f5693510e73cb7a87617447" ) },
-			{ "Obelisk",        new NetworkParameters( "GOV",   "FEW",  "1cfde7c388b9e8ac06462d68aadbd966b58f88797637d9af805b4560b0e9661e" ) },
-			{ "Base",      new NetworkParameters( "TEST",  "TEST", "f34fcd74a151676cfce5121f37c4c3257821cd6754239a3f9c63023018cdd710" ) },
-			{ "BaseDev",   new NetworkParameters( "TEST",  "TEST", "f34fcd74a151676cfce5121f37c4c3257821cd6754239a3f9c63023018cdd710" ) },
-			{ "PPY2T",          new NetworkParameters( "PPY2T", "PPY",  "ecbdc74d20aa1d4bab31b7c2152f505cf0c732e69c55643319cd8e41960f43df" ) }
+			{ "BitShares",		new NetworkParameters( "BTS",   "BTS",  "4018d7844c78f6a6c41c6a552b898022310fc5dec06da467ee7905a8dad512c8" ) },
+			{ "Muse",			new NetworkParameters( "MUSE",  "MUSE", "45ad2d3f9ef92a49b55c2227eb06123f613bb35dd08bd876f2aea21925a67a67" ) },
+			{ "Test",			new NetworkParameters( "TEST",  "TEST", "39f5e2ede1f8bc1a3a54a7914414e3779e33193f1f5693510e73cb7a87617447" ) },
+			{ "Obelisk",		new NetworkParameters( "GOV",   "FEW",  "1cfde7c388b9e8ac06462d68aadbd966b58f88797637d9af805b4560b0e9661e" ) },
+			{ "Base",			new NetworkParameters( "TEST",  "TEST", "f34fcd74a151676cfce5121f37c4c3257821cd6754239a3f9c63023018cdd710" ) },
+			{ "BaseDev",		new NetworkParameters( "TEST",  "TEST", "f34fcd74a151676cfce5121f37c4c3257821cd6754239a3f9c63023018cdd710" ) },
+			{ "PPY2T",			new NetworkParameters( "PPY2T", "PPY",  "ecbdc74d20aa1d4bab31b7c2152f505cf0c732e69c55643319cd8e41960f43df" ) },
+			{ "PPYTEST",		new NetworkParameters( "PPYTEST", "PPYTEST",  "be6b79295e728406cbb7494bcb626e62ad278fa4018699cf8f75739f4c1a81fd" ) }
 		};
 
 		static string coreAsset = "CORE";
